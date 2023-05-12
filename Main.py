ea2b187e881a5ae8b592e6486a360a701622edc5
Sentientnapkin <73544597+Sentientnapkin@users.noreply.github.com>
2023-05-12 14:48:04 -0700

    Working on plotting it

---
+++ Main.py
@@ -0,0 +1,4 @@
+import MicroevolutionaryProcesses as mp
+
+# Create a population of 20 individuals, half 'A' and half 'a'
+mp.natural_selection(['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'], 20)
--- MicroevolutionaryProcesses.py
+++ MicroevolutionaryProcesses.py
@@ -1,22 +1,27 @@
 import numpy as np
+import random
+import matplotlib.pyplot as plt
+
 
 def natural_selection(alleles, generations):
+    dom_list = []
+    res_list = []
     for gen in range(generations):
         pairs = []
-        while alleles.size() > 0:
-            allele_dom_count = 0
-            allele_res_count = 0
-
-            allele1index = np.random.randint(0, alleles.size())
-            allele1 = alleles[allele1index]
+        allele_dom_count = 0
+        allele_res_count = 0
+        while len(alleles) > 0:
+            allele1 = random.choice(alleles)
             if allele1 == 'A':
                 allele_dom_count += 1
             else:
                 allele_res_count += 1
             alleles.remove(allele1)
 
-            allele2index = np.random.randint(0, alleles.size())
-            allele2 = alleles[allele2index]
+            if len(alleles) == 0:
+                break
+
+            allele2 = random.choice(alleles)
             if allele2 == 'A':
                 allele_dom_count += 1
             else:
@@ -25,8 +30,13 @@ def natural_selection(alleles, generations):
 
             pairs.append([allele1, allele2])
 
-            allele_dom_freq = allele_dom_count / (allele_dom_count + allele_res_count)
-            allele_res_freq = allele_res_count / (allele_dom_count + allele_res_count)
+        allele_dom_freq = allele_dom_count / (allele_dom_count + allele_res_count)
+        dom_list.append(allele_dom_freq)
+        allele_res_freq = allele_res_count / (allele_dom_count + allele_res_count)
+        res_list.append(allele_res_freq)
+        plt.plot(dom_list)
+        plt.plot(res_list)
+        plt.pause(0.5)
 
         next_generation = []
         for pair in pairs:
@@ -36,36 +46,40 @@ def natural_selection(alleles, generations):
             elif pair[0] != pair[1]:
                 next_generation.append('A')
                 next_generation.append('a')
-                random = np.random.randint(0, 2)
-                if random == 0:
+                rand = np.random.randint(0, 2)
+                if rand == 0:
                     next_generation.append('A')
                 else:
                     next_generation.append('a')
 
         alleles = next_generation
 
+    plt.show()
     return alleles
+
 
 def sexual_selection(alleles):
     print("Sexual Selection")
 
+
 def artificial_selection(alleles, generations):
     for gen in range(generations):
+        print(alleles)
         pairs = []
-        while alleles.size() > 0:
-            allele_dom_count = 0
-            allele_res_count = 0
-
-            allele1index = np.random.randint(0, alleles.size())
-            allele1 = alleles[allele1index]
+        allele_dom_count = 0
+        allele_res_count = 0
+        while len(alleles) > 0:
+            allele1 = random.choice(alleles)
             if allele1 == 'A':
                 allele_dom_count += 1
             else:
                 allele_res_count += 1
             alleles.remove(allele1)
 
-            allele2index = np.random.randint(0, alleles.size())
-            allele2 = alleles[allele2index]
+            if len(alleles) == 0:
+                break
+
+            allele2 = random.choice(alleles)
             if allele2 == 'A':
                 allele_dom_count += 1
             else:
@@ -74,8 +88,8 @@ def artificial_selection(alleles, generations):
 
             pairs.append([allele1, allele2])
 
-            allele_dom_freq = allele_dom_count / (allele_dom_count + allele_res_count)
-            allele_res_freq = allele_res_count / (allele_dom_count + allele_res_count)
+        allele_dom_freq = allele_dom_count / (allele_dom_count + allele_res_count)
+        allele_res_freq = allele_res_count / (allele_dom_count + allele_res_count)
 
         next_generation = []
         for pair in pairs:
@@ -90,11 +104,14 @@ def artificial_selection(alleles, generations):
 
     return alleles
 
+
 def founder_effect(alleles, generations):
     return genetic_drift(alleles, generations, np.random.randint(3, 5))
 
+
 def bottleneck_effect(alleles, generations):
     return genetic_drift(alleles, generations, 4)
+
 
 def genetic_drift(alleles, generations, survivor_count):
     alleles = standard_reproduction(alleles)
@@ -107,32 +124,24 @@ def genetic_drift(alleles, generations, survivor_count):
 
     allele_dom_count = 0
     allele_res_count = 0
-    allele_mut_count = 0
     pairs = []
     for i in range(10):
-        allele1index = np.random.randint(0, len(alleles))
-        allele1 = alleles[allele1index]
+        allele1 = random.choice(alleles)
         if allele1 == 'A':
             allele_dom_count += 1
-        elif allele1 == 'a':
-            allele_res_count += 1
         else:
-            allele_mut_count += 1
+            allele_res_count += 1
 
-        allele2index = np.random.randint(0, len(alleles))
-        allele2 = alleles[allele2index]
+        allele2 = random.choice(alleles)
         if allele2 == 'A':
             allele_dom_count += 1
-        elif allele2 == 'a':
-            allele_res_count += 1
         else:
-            allele_mut_count += 1
+            allele_res_count += 1
 
         pairs.append([allele1, allele2])
 
-    allele_dom_freq = allele_dom_count / (allele_dom_count + allele_res_count + allele_mut_count)
-    allele_res_freq = allele_res_count / (allele_dom_count + allele_res_count + allele_mut_count)
-    allele_mut_freq = allele_mut_count / (allele_dom_count + allele_res_count + allele_mut_count)
+    allele_dom_freq = allele_dom_count / (allele_dom_count + allele_res_count)
+    allele_res_freq = allele_res_count / (allele_dom_count + allele_res_count)
 
     next_generation = []
     for pair in pairs:
@@ -140,33 +149,34 @@ def genetic_drift(alleles, generations, survivor_count):
             for i in range(3):
                 next_generation.append(pair[0])
         else:
-            next_generation.append(pairs[0])
-            next_generation.append(pairs[1])
-            random = np.random.randint(0, 2)
-            next_generation.append(pairs[random])
+            next_generation.append(pair[0])
+            next_generation.append(pair[1])
+            rand = np.random.randint(0, 2)
+            next_generation.append(pair[rand])
 
     alleles = next_generation
 
     for gen in range(generations - 2):
         alleles = standard_reproduction(alleles)
 
-def gene_flow_wrapper(alleles, generations):
-    alleles = gene_flow(alleles)
+
+def gene_flow(alleles, generations):
+    alleles = gene_flow_wrapper(alleles)
     for i in range(6):
         alleles.append('B')
 
-    for gen in range(generations-1):
-        alleles = gene_flow(alleles)
+    for gen in range(generations - 1):
+        alleles = gene_flow_wrapper(alleles)
 
-def gene_flow(alleles):
+
+def gene_flow_wrapper(alleles):
     pairs = []
+    allele_dom_count = 0
+    allele_res_count = 0
+    allele_new_count = 0
+    print(alleles)
     while len(alleles) > 0:
-        allele_dom_count = 0
-        allele_res_count = 0
-        allele_new_count = 0
-
-        allele1index = np.random.randint(0, len(alleles))
-        allele1 = alleles[allele1index]
+        allele1 = random.choice(alleles)
         if allele1 == 'A':
             allele_dom_count += 1
         elif allele1 == 'a':
@@ -175,8 +185,10 @@ def gene_flow(alleles):
             allele_new_count += 1
         alleles.remove(allele1)
 
-        allele2index = np.random.randint(0, len(alleles))
-        allele2 = alleles[allele2index]
+        if len(alleles) == 0:
+            break
+
+        allele2 = random.choice(alleles)
         if allele2 == 'A':
             allele_dom_count += 1
         elif allele2 == 'a':
@@ -187,9 +199,12 @@ def gene_flow(alleles):
 
         pairs.append([allele1, allele2])
 
-        allele_dom_freq = allele_dom_count / (allele_dom_count + allele_res_count + allele_new_count)
-        allele_res_freq = allele_res_count / (allele_dom_count + allele_res_count + allele_new_count)
-        allele_new_freq = allele_new_count / (allele_dom_count + allele_res_count + allele_new_count)
+    allele_dom_freq = allele_dom_count / (allele_dom_count + allele_res_count + allele_new_count)
+    allele_res_freq = allele_res_count / (allele_dom_count + allele_res_count + allele_new_count)
+    allele_new_freq = allele_new_count / (allele_dom_count + allele_res_count + allele_new_count)
+    print(allele_dom_freq)
+    print(allele_res_freq)
+    print(allele_new_freq)
 
     next_generation = []
     for pair in pairs:
@@ -197,33 +212,34 @@ def gene_flow(alleles):
             for i in range(3):
                 next_generation.append(pair[0])
         else:
-            next_generation.append(pairs[0])
-            next_generation.append(pairs[1])
-            random = np.random.randint(0, 2)
-            next_generation.append(pairs[random])
+            next_generation.append(pair[0])
+            next_generation.append(pair[1])
+            rand = np.random.randint(0, 2)
+            next_generation.append(pair[rand])
 
     alleles = next_generation
     return alleles
 
-def mutation_wrapper(alleles, generations):
-    alleles = mutation(alleles)
-    alleles.remove(np.random.randint(0, len(alleles)))
+
+def mutation(alleles, generations):
+    alleles = mutation_wrapper(alleles)
+    alleles.remove(random.choice(alleles))
     alleles.append('M')
 
-    for gen in range(generations-1):
-        alleles = mutation(alleles)
+    for gen in range(generations - 1):
+        alleles = mutation_wrapper(alleles)
 
     return alleles
 
-def mutation(alleles):
-    pairs = []
-    while len(alleles) > 0:
-        allele_dom_count = 0
-        allele_res_count = 0
-        allele_mut_count = 0
 
-        allele1index = np.random.randint(0, len(alleles))
-        allele1 = alleles[allele1index]
+def mutation_wrapper(alleles):
+    pairs = []
+    allele_dom_count = 0
+    allele_res_count = 0
+    allele_mut_count = 0
+    print(alleles)
+    while len(alleles) > 0:
+        allele1 = random.choice(alleles)
         if allele1 == 'A':
             allele_dom_count += 1
         elif allele1 == 'a':
@@ -232,8 +248,10 @@ def mutation(alleles):
             allele_mut_count += 1
         alleles.remove(allele1)
 
-        allele2index = np.random.randint(0, len(alleles))
-        allele2 = alleles[allele2index]
+        if len(alleles) == 0:
+            break
+
+        allele2 = random.choice(alleles)
         if allele2 == 'A':
             allele_dom_count += 1
         elif allele2 == 'a':
@@ -244,9 +262,12 @@ def mutation(alleles):
 
         pairs.append([allele1, allele2])
 
-        allele_dom_freq = allele_dom_count / (allele_dom_count + allele_res_count + allele_mut_count)
-        allele_res_freq = allele_res_count / (allele_dom_count + allele_res_count + allele_mut_count)
-        allele_mut_freq = allele_mut_count / (allele_dom_count + allele_res_count + allele_mut_count)
+    allele_dom_freq = allele_dom_count / (allele_dom_count + allele_res_count + allele_mut_count)
+    allele_res_freq = allele_res_count / (allele_dom_count + allele_res_count + allele_mut_count)
+    allele_mut_freq = allele_mut_count / (allele_dom_count + allele_res_count + allele_mut_count)
+    print(allele_dom_freq)
+    print(allele_res_freq)
+    print(allele_mut_freq)
 
     next_generation = []
     for pair in pairs:
@@ -254,33 +275,35 @@ def mutation(alleles):
             for i in range(3):
                 next_generation.append(pair[0])
         else:
-            next_generation.append(pairs[0])
-            next_generation.append(pairs[1])
+            next_generation.append(pair[0])
+            next_generation.append(pair[1])
             if pair[0] == 'M' or pair[1] == 'M':
                 next_generation.append('M')
             else:
-                random = np.random.randint(0, 2)
-                next_generation.append(pairs[random])
+                rand = np.random.randint(0, 2)
+                next_generation.append(pair[rand])
 
     alleles = next_generation
     return alleles
 
-def standard_reproduction(alleles):
-    pairs = []
-    while len(alleles) > 0:
-        allele_dom_count = 0
-        allele_res_count = 0
 
-        allele1index = np.random.randint(0, len(alleles))
-        allele1 = alleles[allele1index]
+def standard_reproduction(alleles):
+    print(alleles)
+    pairs = []
+    allele_dom_count = 0
+    allele_res_count = 0
+    while len(alleles) > 0:
+        allele1 = random.choice(alleles)
         if allele1 == 'A':
             allele_dom_count += 1
         elif allele1 == 'a':
             allele_res_count += 1
         alleles.remove(allele1)
 
-        allele2index = np.random.randint(0, len(alleles))
-        allele2 = alleles[allele2index]
+        if len(alleles) == 0:
+            break
+
+        allele2 = random.choice(alleles)
         if allele2 == 'A':
             allele_dom_count += 1
         elif allele2 == 'a':
@@ -289,8 +312,9 @@ def standard_reproduction(alleles):
 
         pairs.append([allele1, allele2])
 
-        allele_dom_freq = allele_dom_count / (allele_dom_count + allele_res_count)
-        allele_res_freq = allele_res_count / (allele_dom_count + allele_res_count)
+    allele_dom_freq = allele_dom_count / (allele_dom_count + allele_res_count)
+    allele_res_freq = allele_res_count / (allele_dom_count + allele_res_count)
+    print(allele_dom_freq, allele_res_freq)
 
     next_generation = []
     for pair in pairs:
@@ -298,10 +322,10 @@ def standard_reproduction(alleles):
             for i in range(3):
                 next_generation.append(pair[0])
         else:
-            next_generation.append(pairs[0])
-            next_generation.append(pairs[1])
-            random = np.random.randint(0, 2)
-            next_generation.append(pairs[random])
+            next_generation.append(pair[0])
+            next_generation.append(pair[1])
+            rand = np.random.randint(0, 2)
+            next_generation.append(pair[rand])
 
     alleles = next_generation
     return alleles
